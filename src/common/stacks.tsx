18b2e4b6a1bac233438119fd2fbf1fd4c2bb0076
arthagp <arthagusfi11@gmail.com>
2024-02-07 10:27:11 +0700

    added stack

--- src/common/stacks.tsx
+++ src/common/stacks.tsx
@@ -4,39 +4,49 @@ cara penggunaan jika nanti fetch data berisi array of number. kenapa number kare
 contoh :
 
 */
+import { BiLogoPostgresql } from "react-icons/bi";
 import {
-  SiAstro,
-  SiChakraui,
   SiCss3,
-  SiExpo,
   SiExpress,
-  SiFirebase,
   SiFramer,
-  SiGithub,
-  SiGraphql,
-  SiGulp,
   SiHtml5,
   SiJavascript,
   SiJest,
-  SiJson,
-  SiMui,
   SiNextdotjs,
   SiNodedotjs,
-  SiNuxtdotjs,
   SiPhp,
   SiPrisma,
   SiReact,
-  SiReacthookform,
-  SiReactquery,
   SiReactrouter,
-  SiReacttable,
   SiRedux,
-  SiSass,
-  SiStorybook,
   SiTailwindcss,
   SiTypescript,
   SiVite,
   SiVuedotjs,
+  SiLaravel,
+  SiCypress,
+  SiDocker,
+  SiVercel,
 } from "react-icons/si";
 
-export const STACKS = [<SiGithub />, <SiPhp />, <SiRedux />, <SiVite />];
+export const STACKS: JSX.Element[] = [
+  <SiNextdotjs />, //0. NextJs
+  <SiReact />, //1. Reactjs
+  <SiExpress />, //2. Express
+  <BiLogoPostgresql />, //3. PostgreSQL
+  <SiTailwindcss />, //4. Tailwindcss
+  <SiJavascript />, //5. Axios
+  <SiDocker />, //6. Docker
+  <SiVuedotjs />, //7. Vuejs
+  <SiCypress />, //8. Cypress
+  <SiHtml5 />, //9. HTML
+  <SiCss3 />, //10. CSS
+  <SiJavascript />, //11. JS
+  <SiPhp />, //12. PHP
+  <SiLaravel />, //13. Laravel
+  <SiRedux />, //14. Redux
+  <SiVercel />, //15. Vercel
+  <SiReactrouter />, //16. ReactRouter
+  <SiTypescript />, //17. Typescript
+  <SiFramer />, //18. Framer
+];
